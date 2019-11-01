1ca88d601746ca3090d453aa2553d33e29ad7379
lijianfeng <lijianfeng@lonntec.com>
2019-11-01 18:16:28 +0800

    走马灯组件修改

--- lib/material/carousel/carousel.dart
+++ lib/material/carousel/carousel.dart
@@ -95,8 +95,8 @@ class _CarouselState extends State<Carousel>
         decoration: BoxDecoration(
             shape: BoxShape.circle,
             color: currentIndex == index
-                ? Color(0xffcccccc)
-                : Color.fromRGBO(0, 0, 0, 0.4)),
+                ? Theme.of(context).primaryColor
+                : Color(0xffcccccc)),
       ));
     });
     return dots;
@@ -107,10 +107,14 @@ class _CarouselState extends State<Carousel>
       width: widget.slideWidth ?? null,
       child: Stack(
         children: <Widget>[
-          AspectRatio(
-            aspectRatio: aspectRatio,
-            child: child,
-          ),
+          widget.isGrid == true
+              ? Container(
+                  child: child,
+                )
+              : AspectRatio(
+                  aspectRatio: aspectRatio,
+                  child: child,
+                ),
           Positioned(
             bottom: 0.0,
             left: 0.0,
@@ -128,86 +132,121 @@ class _CarouselState extends State<Carousel>
 
   @override
   Widget build(BuildContext context) {
-    return getWrapper(PageView.builder(
-      physics: PageScrollPhysics(),
-      scrollDirection:
-          widget.vertical == false ? Axis.horizontal : Axis.vertical,
-      controller: pageController,
-      itemCount: widget.infinite ? null : widget.items.length,
-      onPageChanged: (int index) {
-        int currentPage = _getRealIndex(
-            index + widget.selectedIndex, realPage, widget.items.length);
-
-        if (widget.beforeChange != null) {
-          widget.beforeChange({'from': currentIndex, 'to': currentPage});
-        }
-        setState(() {
-          currentIndex = currentPage;
-        });
-
-        if (widget.afterChange != null) {
-          widget.afterChange(currentPage);
-        }
-      },
-      itemBuilder: (BuildContext context, int i) {
-        final int index = _getRealIndex(
-            i + widget.selectedIndex, realPage, widget.items.length);
-
-        return Container(
-          margin: EdgeInsets.symmetric(
-              vertical: widget.vertical == true
-                  ? widget.cellSpacing != null ? widget.cellSpacing : 0.0
-                  : 0.0,
-              horizontal: widget.vertical == true
-                  ? 0.0
-                  : widget.cellSpacing != null ? widget.cellSpacing : 0.0),
-          child: AnimatedBuilder(
-            animation: pageController,
-            child: widget.items[index],
-            builder: (BuildContext context, child) {
-              // on the first render, the pageController.page is null,
-              // this is a dirty hack
-              if (pageController.position.minScrollExtent == null ||
-                  pageController.position.maxScrollExtent == null) {
-                Future.delayed(Duration(microseconds: 1), () {
-                  setState(() {});
+    return getWrapper(ScrollConfiguration(
+      behavior: ScrollViewBehavior(false, false, Colors.blue),
+      child: PageView.builder(
+        physics: PageScrollPhysics(),
+        scrollDirection:
+            widget.vertical == false ? Axis.horizontal : Axis.vertical,
+        controller: pageController,
+        itemCount: widget.infinite ? null : widget.items.length,
+        onPageChanged: (int index) {
+          if (widget.infinite == false && widget.autoplay == true) {
+            if (index == widget.items.length - 1) {
+              setState(() {
+                currentIndex = index;
+              });
+              Future.delayed(
+                  Duration(milliseconds: widget.autoplayInterval.toInt()), () {
+                timer.cancel();
+                pageController.animateToPage(0,
+                    duration: playAnimation, curve: widget.easing);
+                setState(() {
+                  currentIndex = 0;
                 });
-                return Container();
+                timer = getTimer();
+              });
+            } else {
+              int currentPage = _getRealIndex(
+                  index + widget.selectedIndex, realPage, widget.items.length);
+
+              if (widget.beforeChange != null) {
+                widget.beforeChange({'from': currentIndex, 'to': currentPage});
               }
-              double value = pageController.page - i;
-              value = (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
-              final double distortionValue =
-                  widget.frameOverflow ? Curves.easeOut.transform(value) : 1.0;
+              setState(() {
+                currentIndex = currentPage;
+              });
 
-              final double height = (widget.slideWidth == null
-                      ? MediaQuery.of(context).size.width
-                      : widget.slideWidth) *
-                  (1 / aspectRatio);
-              final double width = distortionValue *
-                  (widget.slideWidth == null
-                      ? MediaQuery.of(context).size.width
-                      : widget.slideWidth);
+              if (widget.afterChange != null) {
+                widget.afterChange(currentPage);
+              }
+            }
+          } else {
+            int currentPage = _getRealIndex(
+                index + widget.selectedIndex, realPage, widget.items.length);
 
-              return widget.vertical == false
-                  ? Center(
-                      child: SizedBox(
-                        height: widget.isGrid == true
-                            ? null
-                            : distortionValue * height,
-                        child: child,
-                      ),
-                    )
-                  : Center(
-                      child: SizedBox(
-                        height: height,
-                        width: width,
-                        child: child,
-                      ),
-                    );
-            },
-          ),
-        );
-      },
+            if (widget.beforeChange != null) {
+              widget.beforeChange({'from': currentIndex, 'to': currentPage});
+            }
+            setState(() {
+              currentIndex = currentPage;
+            });
+
+            if (widget.afterChange != null) {
+              widget.afterChange(currentPage);
+            }
+          }
+        },
+        itemBuilder: (BuildContext context, int i) {
+          final int index = _getRealIndex(
+              i + widget.selectedIndex, realPage, widget.items.length);
+          return Container(
+            margin: EdgeInsets.symmetric(
+                vertical: widget.vertical == true
+                    ? widget.cellSpacing != null ? widget.cellSpacing : 0.0
+                    : 0.0,
+                horizontal: widget.vertical == true
+                    ? 0.0
+                    : widget.cellSpacing != null ? widget.cellSpacing : 0.0),
+            child: AnimatedBuilder(
+              animation: pageController,
+              child: widget.items[index],
+              builder: (BuildContext context, child) {
+                // on the first render, the pageController.page is null,
+                // this is a dirty hack
+                if (pageController.position.minScrollExtent == null ||
+                    pageController.position.maxScrollExtent == null) {
+                  Future.delayed(Duration(microseconds: 1), () {
+                    setState(() {});
+                  });
+                  return Container();
+                }
+                double value = pageController.page - i;
+                value = (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
+                final double distortionValue = widget.frameOverflow
+                    ? Curves.easeOut.transform(value)
+                    : 1.0;
+
+                final double height = (widget.slideWidth == null
+                        ? MediaQuery.of(context).size.width
+                        : widget.slideWidth) *
+                    (1 / aspectRatio);
+                final double width = distortionValue *
+                    (widget.slideWidth == null
+                        ? MediaQuery.of(context).size.width
+                        : widget.slideWidth);
+
+                return widget.vertical == false
+                    ? Center(
+                        child: SizedBox(
+                          height: widget.isGrid == true
+                              ? null
+                              : distortionValue * height,
+                          child: child,
+                        ),
+                      )
+                    : Center(
+                        child: SizedBox(
+                          height: height,
+                          width: width,
+                          child: child,
+                        ),
+                      );
+              },
+            ),
+          );
+        },
+      ),
     ));
   }
 }
@@ -220,4 +259,32 @@ int _getRealIndex(int position, int base, int length) {
 int _remainder(int input, int source) {
   final int result = input % source;
   return result < 0 ? source + result : result;
+}
+
+///可去掉过度滑动时ListView顶部的蓝色光晕效果
+class ScrollViewBehavior extends ScrollBehavior {
+  final bool isShowLeadingGlow;
+  final bool isShowTrailingGlow;
+  final Color _kDefaultGlowColor;
+
+  ScrollViewBehavior(
+      this.isShowLeadingGlow, this.isShowTrailingGlow, this._kDefaultGlowColor);
+
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    //如果头部或底部有一个 不需要 显示光晕时 返回GlowingOverscrollIndicator
+    if (!isShowLeadingGlow || !isShowTrailingGlow) {
+      return new GlowingOverscrollIndicator(
+        showLeading: isShowLeadingGlow,
+        showTrailing: isShowTrailingGlow,
+        child: child,
+        axisDirection: axisDirection,
+        color: _kDefaultGlowColor,
+      );
+    } else {
+      //都需要光晕时  返回系统默认
+      return super.buildViewportChrome(context, child, axisDirection);
+    }
+  }
 }

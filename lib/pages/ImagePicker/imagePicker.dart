39622c11eb680accb0452d4719734fb455f1d279
lijianfeng <lijianfeng@lonntec.com>
2019-11-22 18:18:52 +0800

    imagepicker 组件还差multiple属性

--- lib/pages/ImagePicker/imagePicker.dart
+++ lib/pages/ImagePicker/imagePicker.dart
@@ -9,66 +9,6 @@ class PageImagePicker extends StatefulWidget {
 }
 
 class _PageImagePickerState extends State<PageImagePicker> {
-  List<Asset> images = List<Asset>();
-  String _error = 'No Error Dectected';
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Widget buildGridView() {
-    return GridView.count(
-      crossAxisCount: 3,
-      children: List.generate(images.length, (index) {
-        Asset asset = images[index];
-        return AssetThumb(
-          asset: asset,
-          width: 300,
-          height: 300,
-        );
-      }),
-    );
-  }
-
-  Future<void> loadAssets() async {
-    List<Asset> resultList = List<Asset>();
-    String error = 'No Error Dectected';
-
-    try {
-      resultList = await MultiImagePicker.pickImages(
-        maxImages: 300,
-        enableCamera: true,
-        selectedAssets: images,
-        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
-        materialOptions: MaterialOptions(
-          actionBarColor: "#abcdef",
-          actionBarTitle: "Example App",
-          allViewTitle: "All Photos",
-          useDetailsView: false,
-          selectCircleStrokeColor: "#000000",
-        ),
-      );
-
-      for (var r in resultList) {
-        var t = await r.filePath;
-        print(t);
-      }
-    } on Exception catch (e) {
-      error = e.toString();
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      images = resultList;
-      _error = error;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,7 +19,6 @@ class _PageImagePickerState extends State<PageImagePicker> {
           child: Container(
             width: 400.0,
             padding: EdgeInsets.symmetric(horizontal: 15.0),
-
             child: AntImagePicker(
               files: [
                 {'url': ''},
@@ -100,6 +39,7 @@ class _PageImagePickerState extends State<PageImagePicker> {
                       'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg'
                 },
               ],
+              length: 6,
               // disableDelete: true,
               onChange: (value) {
                 print(value);
@@ -108,18 +48,6 @@ class _PageImagePickerState extends State<PageImagePicker> {
                 print(value);
               },
             ),
-            // child: Column(
-            //   children: <Widget>[
-            //     Center(child: Text('Error: $_error')),
-            //     RaisedButton(
-            //       child: Text("Pick images"),
-            //       onPressed: loadAssets,
-            //     ),
-            //     Expanded(
-            //       child: buildGridView(),
-            //     )
-            //   ],
-            // ),
           ),
         ));
   }
